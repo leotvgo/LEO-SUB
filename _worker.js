f937d7895ff252c3b0bb96402a264425e2044f46
leotvgo <160683122+leotvgo@users.noreply.github.com>
2025-02-20 16:13:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -21,8 +21,8 @@ https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub
 `
 
 let urls = [];
-let subConverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConverter = "SUBAPI.dyzh.leo123.cn"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConfig = "https://xn--tp5a4a.us.kg/https://raw.githubusercontent.com/leotvgo/duo/refs/heads/main/guize/leo.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
